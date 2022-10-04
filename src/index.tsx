f125d05465b32a7b79d26b05212aa49ebf8411c6
Laetitia Hars <laetitia.hars@gmail.com>
2022-10-04 13:55:30 +0200

    :sparkles: save and load data from localStorage

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,9 @@ import Header from "./pages/Header";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import CreateEmployee from "./pages/CreateEmployee";
 import EmployeeList from "./pages/EmployeeList";
-import store from "./utils/store";
 import { Provider } from "react-redux";
+import { persistor, store } from "./utils/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -14,16 +15,17 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/create-employee" element={<CreateEmployee />}></Route>
-          <Route path="/employee-list" element={<EmployeeList />}></Route>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/create-employee" element={<CreateEmployee />}></Route>
+            <Route path="/employee-list" element={<EmployeeList />}></Route>
 
-          <Route path="*" element={<CreateEmployee />} />
-        </Routes>
-        {/*<Footer />*/}
-      </Router>
+            <Route path="*" element={<CreateEmployee />} />
+          </Routes>
+        </Router>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,13 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import storage from "redux-persist/lib/storage";
 import employeeReducer from "../features/employee/employee";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const reducers = combineReducers({
+  employee: employeeReducer,
+});
+
+const persistedReducer = persistReducer<any, any>(persistConfig, reducers);
 
 export const store = configureStore({
-  reducer: {
-    employee: employeeReducer,
-  },
-  devTools: true,
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: [thunk],
 });
+
+export const persistor = persistStore(store);
 
 export default store;
 
